c0d988d0090e2797f6fb52735c255be3cdcb0041
Georgii <georgii.krikun@celantur.com>
2025-04-10 19:50:41 +0200

    add lsp

--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -2,6 +2,13 @@ plugins = {
   {
     "github/copilot.vim",
     lazy = false
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "plugins.configs.lspconfig"
+      require "plugins.configs.lspconfig"
+    end,
   }
 }
 
